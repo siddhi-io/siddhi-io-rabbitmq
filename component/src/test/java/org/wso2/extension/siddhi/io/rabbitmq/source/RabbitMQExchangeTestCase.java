e3067a5c65bd0799c010e5feeac96dadef84acf5
Suho <suhothayan@gmail.com>
2017-08-29 10:59:35 +0530

    Revert "Adding test cases"

--- component/src/test/java/org/wso2/extension/siddhi/io/rabbitmq/source/RabbitMQExchangeTestCase.java
+++ component/src/test/java/org/wso2/extension/siddhi/io/rabbitmq/source/RabbitMQExchangeTestCase.java
@@ -2,7 +2,7 @@ package org.wso2.extension.siddhi.io.rabbitmq.source;
 
 
 import org.apache.log4j.Logger;
-import org.testng.Assert;
+import org.testng.AssertJUnit;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;;
 import org.wso2.siddhi.core.SiddhiAppRuntime;
@@ -72,8 +72,8 @@ public class RabbitMQExchangeTestCase {
         arrayList.add(new Event(System.currentTimeMillis(), new Object[]{"WSO2", 57.6f, 100L}));
         fooStream.send(arrayList.toArray(new Event[3]));
         Thread.sleep(10000);
-        Assert.assertEquals(3, count);
-        Assert.assertTrue(eventArrived);
+        AssertJUnit.assertEquals(3, count);
+        AssertJUnit.assertTrue(eventArrived);
 
         executionPlanRuntime.shutdown();
         siddhiAppRuntime.shutdown();
@@ -127,19 +127,18 @@ public class RabbitMQExchangeTestCase {
         arrayList.add(new Event(System.currentTimeMillis(), new Object[]{"WSO2", 57.6f, 100L}));
         fooStream.send(arrayList.toArray(new Event[3]));
         Thread.sleep(10000);
-        Assert.assertEquals(3, count);
-        Assert.assertTrue(eventArrived);
+        AssertJUnit.assertEquals(3, count);
+        AssertJUnit.assertTrue(eventArrived);
 
         executionPlanRuntime.shutdown();
         siddhiAppRuntime.shutdown();
 
     }
-
     @Test
     public void rabbitmqWithoutExchangeNameTest() throws InterruptedException {
         try {
             log.info("---------------------------------------------------------------------------------------------");
-            log.info("RabbitMQ Source test without exchange name");
+            log.info("RabbitMQ Sink and Source test without exchange name");
             log.info("---------------------------------------------------------------------------------------------");
             SiddhiManager siddhiManager = new SiddhiManager();
             SiddhiAppRuntime siddhiAppRuntime = siddhiManager
@@ -153,7 +152,41 @@ public class RabbitMQExchangeTestCase {
                                     "@map(type='xml'))" +
                                     "Define stream BarStream1 (symbol string, price float, volume long);" +
                                     "from FooStream1 select symbol, price, volume insert into BarStream1;");
+            siddhiAppRuntime.addCallback("BarStream1", new StreamCallback() {
+                @Override
+                public void receive(Event[] events) {
+                    for (Event event : events) {
+                        log.info(event);
+                        eventArrived = true;
+                        count++;
+                    }
+                }
+            });
+
             siddhiAppRuntime.start();
+            SiddhiAppRuntime executionPlanRuntime = siddhiManager.createSiddhiAppRuntime(
+                    "@App:name('TestExecutionPlan') " +
+                            "define stream FooStream1 (symbol string, price float, volume long); " +
+                            "@info(name = 'query1') " +
+                            "@sink(type ='rabbitmq', uri ='amqp://guest:guest@172.17.0.2:5672', " +
+                            "exchange.type='topic', exchange.durable.enabled= 'true', " +
+                            "routing.key= 'topic.test', " +
+                            "@map(type='xml'))" +
+                            "Define stream BarStream1 (symbol string, price float, volume long);" +
+                            "from FooStream1 select symbol, price, volume insert into BarStream1;");
+            InputHandler fooStream = executionPlanRuntime.getInputHandler("FooStream1");
+
+            executionPlanRuntime.start();
+            ArrayList<Event> arrayList = new ArrayList<Event>();
+            arrayList.add(new Event(System.currentTimeMillis(), new Object[]{"WSO2", 55.6f, 100L}));
+            arrayList.add(new Event(System.currentTimeMillis(), new Object[]{"IBM", 75.6f, 100L}));
+            arrayList.add(new Event(System.currentTimeMillis(), new Object[]{"WSO2", 57.6f, 100L}));
+            fooStream.send(arrayList.toArray(new Event[3]));
+            Thread.sleep(10000);
+            AssertJUnit.assertEquals(3, count);
+            AssertJUnit.assertTrue(eventArrived);
+
+            executionPlanRuntime.shutdown();
             siddhiAppRuntime.shutdown();
         } catch (Exception e) {
             log.warn("Exchange name is not mentioned");
